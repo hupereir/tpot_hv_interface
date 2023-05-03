26b15928b67204b59576be9311929deed84e8271
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2023-05-03 16:29:08 -0400

    test setting and reading v0

--- python/tpot_read_v0set.py
+++ python/tpot_read_v0set.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 import ctypes
+import pathlib
 
 # Load the shared library into ctypes
-libname = "/home/hpereira/sphenix/src/CAEN/caen_hv_reader/install/lib/libcaen_hv_interface.so"
+libname = "libcaen_hv_interface.so"
 c_lib = ctypes.CDLL(libname)
 
-answer = c_lib.connect_to_interface()
+# connect
+answer = c_lib.connect_to_interface( b'10.20.34.154', b'admin', b'admin')
+if answer == 0:
+    print( "Unable to connect" )
+    exit
+
+c_lib.get_v0set.restype = ctypes.c_float
+
+c_lib.set_v0set(b'NEZ_R1', ctypes.c_float(100.0))
+answer = c_lib.get_v0set(b'NEZ_R1')
 print( f"answer: {answer}" )
 
-slot=0
-channel=0
-answer = c_lib.get_v0set(slot,channel)
+#disconnect
+c_lib.disconnect_from_interface()
