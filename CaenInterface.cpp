b719cb79d6f1f4f276b596aec09001aa0c2a5a84
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2023-05-03 17:09:18 -0400

    added a way to track wether last command was succesfull

--- CaenInterface.cpp
+++ CaenInterface.cpp
@@ -5,20 +5,23 @@
 
 static Connection m_connection;
 static std::string m_json_output;
-  
+
 extern "C" {
   
   bool connect_to_interface( 
-			    const char* /*ip*/,
-			    const char* /*user*/,
-			    const char* /*password*/ ); 
-
+    const char* /*ip*/,
+    const char* /*user*/,
+    const char* /*password*/ ); 
+  
   void disconnect_from_interface();
-
+  
   float get_v0set( const char* /*channel name*/ );
   void set_v0set( const char* /*channel name*/, float /*value*/ );
-
+  
   const char* get_channel_status();
+  
+  bool last_command_successful();
+
 }
 
 //___________________________________________
@@ -71,30 +74,33 @@ void set_v0set( const char* name, float value )
 //__________________________________________________
 const char* get_channel_status()
 {
-
   std::ostringstream out;
-
+  
   // header
-  out << "// slot, channel, chname, vset, vmon, imon, status, trip" << std::endl;
-  for(const auto& slot:m_connection.get_slots())
-    { 
-      for( const auto& channel:m_connection.get_channels( slot ) )
-	{
-	  out 
-	    << "{ \"slot_id\":" << slot.m_id 
-	    << ", \"ch_id\":" << channel.m_id 
-	    << ", \"ch_name\":\"" << channel.m_name << "\""
-	    << ", \"v0set\":" << channel.m_v0set
-	    << ", \"vmon\":" << channel.m_vmon 
-	    << ", \"imon\":" << channel.m_imon
-	    << ", \"status\":" << channel.m_status
-	    << ", \"trip\":" << channel.m_trip_int 
-	    << " }"
-	    << std::endl;
-	}
+  out << "// slot, channel, chname, vset, vmon, imon, status, trip" << std::endl;  
+  for(const auto& slot:m_connection.get_slots() )
+  {
+    
+    for( const auto& channel:m_connection.get_channels( slot ) )
+    {
+      out 
+        << "{ \"slot_id\":" << slot.m_id 
+        << ", \"ch_id\":" << channel.m_id 
+        << ", \"ch_name\":\"" << channel.m_name << "\""
+        << ", \"v0set\":" << channel.m_v0set
+        << ", \"vmon\":" << channel.m_vmon 
+        << ", \"imon\":" << channel.m_imon
+        << ", \"status\":" << channel.m_status
+        << ", \"trip\":" << channel.m_trip_int 
+        << " }"
+        << std::endl;
     }
-
-  // return "hello";
+  }
+  
   m_json_output = out.str();
   return m_json_output.c_str();
 }
+
+//__________________________________________________
+bool last_command_successful() 
+{ return m_connection.is_connected() && m_connection.last_reply() == CAENHV_OK; }
