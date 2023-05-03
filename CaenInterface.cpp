4073eafaac5336832a13d5a5839678f640f51d2c
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2023-05-03 16:26:52 -0400

    read all channel status and return as a static stream

--- CaenInterface.cpp
+++ CaenInterface.cpp
@@ -1,44 +1,96 @@
 #include "CaenInterface.h"
 #include "Connection.h"
 
+#include <sstream>
+
 static Connection m_connection;
+static std::string m_json_output;
   
 extern "C" {
   
-//   bool connect_to_interface( 
-//     const std::string&,
-//     const std::string&,
-//     const std::string& ); 
-  
-  bool connect_to_interface();
-  float get_v0set( int slot, int channel );
- 
+  bool connect_to_interface( 
+			    const char* /*ip*/,
+			    const char* /*user*/,
+			    const char* /*password*/ ); 
+
+  void disconnect_from_interface();
+
+  float get_v0set( const char* /*channel name*/ );
+  void set_v0set( const char* /*channel name*/, float /*value*/ );
+
+  const char* get_channel_status();
 }
 
 //___________________________________________
 bool connect_to_interface( 
-  const std::string& ip,
-  const std::string& user,
-  const std::string& password )
+  const char* ip,
+  const char* user,
+  const char* password )
 { 
   m_connection.connect( ip, user, password ); 
   return m_connection.is_connected();
 }
 
 //___________________________________________
-bool connect_to_interface()
-{ 
-  return connect_to_interface( "10.20.34.154", "admin", "admin" ); 
-}
-
-//___________________________________________
-void disconnect_from_insterface() 
+void disconnect_from_interface() 
 { m_connection.disconnect(); }
 
 //__________________________________________
-float get_v0set( int slot, int channel )
-{ return get_parameter_value<float>( m_connection.get_handle(), slot, channel, "V0Set" ); }
+float get_v0set( const char* name )
+{ 
+  if( !m_connection.is_connected() ) return 0;
+  const auto reply = m_connection.get_channel_id( name );
+  const auto& slot = reply.first;
+  const auto& channel = reply.second;
+  if( slot >= 0 ) {
+    return get_parameter_value<float>( m_connection.get_handle(), slot, channel, "V0Set" ); 
+  } else {
+    std::cout << "get_v0Set - channel not found: " << name << std::endl;
+    return -1;
+  }
+}
 
 //__________________________________________
-void set_v0( int slot, int channel, float value )
-{ set_parameter_value( m_connection.get_handle(), slot, channel, "V0Set", value ); }
+void set_v0set( const char* name, float value )
+{ 
+  if( !m_connection.is_connected() ) return;
+  const auto reply = m_connection.get_channel_id( name );
+  const auto& slot = reply.first;
+  const auto& channel = reply.second;
+  if( slot >= 0 ) {
+    set_parameter_value<float>( m_connection.get_handle(), slot, channel, "V0Set", value );
+  } else {
+    std::cout << "get_v0Set - channel not found: " << name << std::endl;
+  }
+}
+
+//__________________________________________________
+const char* get_channel_status()
+{
+
+  std::ostringstream out;
+
+  // header
+  out << "// slot, channel, chname, vset, vmon, imon, status, trip" << std::endl;
+  for(const auto& slot:m_connection.get_slots())
+    { 
+      for( const auto& channel:m_connection.get_channels( slot ) )
+	{
+	  out 
+	    << "{ \"slot_id\":" << slot.m_id 
+	    << ", \"ch_id\":" << channel.m_id 
+	    << ", \"ch_name\":\"" << channel.m_name << "\""
+	    << ", \"v0set\":" << channel.m_v0set
+	    << ", \"vmon\":" << channel.m_vmon 
+	    << ", \"imon\":" << channel.m_imon
+	    << ", \"status\":" << channel.m_status
+	    << ", \"trip\":" << channel.m_trip_int 
+	    << " }"
+	    << std::endl;
+	}
+    }
+
+  // return "hello";
+  m_json_output = out.str();
+  return m_json_output.c_str();
+}
