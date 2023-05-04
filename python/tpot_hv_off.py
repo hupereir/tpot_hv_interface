e308c2a16f0e21a5fb3252789f0832d96d88e12e
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2023-05-04 16:08:26 -0400

    improved channel parsing. tpot_hv_on, tpot_hv_off now accept single channels

--- python/tpot_hv_off.py
+++ python/tpot_hv_off.py
@@ -4,29 +4,10 @@ import pathlib
 import sys
 import time
 
-# define default detector names
-det_names_south = ['SEP', 'SEZ', 'SCOP', 'SCOZ', 'SCIP', 'SCIZ', 'SWP', 'SWZ']
-det_names_north = ['NEP', 'NEZ', 'NCOP', 'NCOZ', 'NCIP', 'NCIZ', 'NWP', 'NWZ']
-det_names_all = det_names_south+det_names_north
+from tpot_util import *
 
-# parse arguments
-det_names = set();
-for i, arg in enumerate(sys.argv):
-  if i == 0:
-    continue
-  if arg == 'south' :
-    for name in det_names_south:
-      det_names.add(name)
-  elif arg == 'north':
-    for name in det_names_north:
-      det_names.add( name )
-  elif arg == 'all':
-    for name in det_names_all:
-      det_names.add( name )
-  elif arg in det_names_all:
-    det_names.add( arg )
-  else :
-    print( f'unknown argument: {arg}' )
+# get detector names
+channel_dict = parse_arguments( sys.argv[1:] )
 
 # Load the shared library into ctypes
 path = "/home/phnxrc/hpereira/lib"
@@ -39,13 +20,10 @@ if answer == 0:
     print( "Unable to connect" )
     exit
 
-# define channel suffix
-suffixes = ['_R1', '_R2', '_R3', '_R4', '_D' ]
-for name in det_names:
-  for suffix in suffixes:
-    ch_name = name + suffix
-    c_lib.set_channel_on( bytes(ch_name,'ascii'), 0 )
+for det_name in sorted(channel_dict.keys()):
+  print( f'processing {det_name}' )
+  for channel_name in sorted(channel_dict[det_name] ):
+    c_lib.set_channel_on( bytes(ch_name,'ascii'),0 )
     time.sleep(1)
-
 #disconnect
 c_lib.disconnect_from_interface()
---
+++ python/tpot_util.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# regular expressions
+import re
+
+# parse arguments
+# accepted values are: 
+# all, south, north, 
+# detector name, e.g. NCOZ, SEP
+# channel name, e.g. NCOZ_R1, SEP_D
+#
+# returns a dictionary of all selected channels, grouped by detector names
+def parse_arguments( arguments ):
+
+  # define default detector names
+  det_names_south = {'SEP', 'SEZ', 'SCOP', 'SCOZ', 'SCIP', 'SCIZ', 'SWP', 'SWZ'}
+  det_names_north = {'NEP', 'NEZ', 'NCOP', 'NCOZ', 'NCIP', 'NCIZ', 'NWP', 'NWZ'}
+  det_names_all = det_names_south|det_names_north
+
+  suffixes = {'_R1', '_R2', '_R3', '_R4', '_D' }
+
+  # get all detector/single channel names
+  det_names = set()
+  for arg in arguments:
+    if arg == 'south' :
+      det_names.update(det_names_south)
+    elif arg == 'north':
+      det_names.update(det_names_north)
+    elif arg == 'all':
+      det_names.update(det_names_all)
+    elif arg in det_names_all:
+      det_names.add( arg )
+    else :
+      result = re.fullmatch( '(\w+)_(R[1-4]|D)',arg )
+      if result and result.group(1)  in det_names_all:
+        det_names.add( arg )
+      else: 
+        print( f'unknown argument: {arg}' )
+
+  # build dictionary
+  channel_dict = dict()
+  for name in det_names:
+    if name in det_names_all:
+      channels = set()
+      for suffix in suffixes:
+        channels.add( name+suffix )
+      channel_dict[name] = channels
+    else:
+      result = re.fullmatch( '(\w+)_(R[1-4]|D)',name )
+      module = result.group(1)
+      if module in channel_dict.keys():
+        channel_dict[module].add(name)
+      else:
+        channel_dict[module]={name}
+
+  return channel_dict
