aa5da5291ba6332655093ebff01545f572c283a4
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2023-05-04 16:11:10 -0400

    fix var name

--- python/tpot_hv_off.py
+++ python/tpot_hv_off.py
@@ -22,7 +22,7 @@ if answer == 0:
 
 for det_name in sorted(channel_dict.keys()):
   print( f'processing {det_name}' )
-  for channel_name in sorted(channel_dict[det_name] ):
+  for ch_name in sorted(channel_dict[det_name] ):
     c_lib.set_channel_on( bytes(ch_name,'ascii'),0 )
     time.sleep(1)
 #disconnect
