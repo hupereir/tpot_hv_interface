79eb898e9b182cb5605dc1d67dbc84dc71016485
Hugo Pereira Da Costa <hugo.pereira-da-costa@lanl.gov>
2023-05-20 14:05:58 -0400

    added script to print full channel status of selected channels

---
+++ python/tpot_hv_get_status.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import ctypes
+import pathlib
+import sys
+import time
+import json
+import re
+
+from tpot_hv_util import *
+
+# usage
+if len(sys.argv) == 1:
+  print(
+    'usage: \n'
+    '  tpot_hv_get_status.py south|north|all|<detector names>|<channel names>\n'
+    '\nwith\n'
+    '  <detector names>: a list of detectors for which to get the status, e.g. NCOP SEW ...\n'
+    '  <channel names> : a list of single channels to turn on, e.g. NCOP_D SEW_R1 ...')
+  exit(0)
+
+# get channel names
+channel_dict = parse_arguments( sys.argv[1:] )
+
+# store selected channels
+selected_channels = set()
+for det_name in sorted(channel_dict.keys()):
+  print( f'processing {det_name}' )
+  for ch_name in sorted(channel_dict[det_name] ):
+    selected_channels.add(ch_name)
+
+# Load the shared library into ctypes
+path = "/home/phnxrc/hpereira/lib"
+libname = f"{path}/libtpot_hv_interface.so"
+c_lib = ctypes.CDLL(libname)
+c_lib.get_channel_status.restype = ctypes.c_char_p
+
+# connect
+answer = c_lib.connect_to_interface( b'10.20.34.154', b'admin', b'admin')
+if answer == 0:
+  print( "Unable to connect" )
+  exit(1)
+
+status = str(c_lib.get_channel_status())
+
+# split by channel
+channels_raw = re.findall('\{.*?\}',status)
+
+for channel_raw in channels_raw:
+  # get channel name 
+  ch_name = channel_raw["ch_name"]
+  if not ch_name in selected_channels: 
+    continue  
+
+  # print(f"{status}")
+  json_object = json.loads(channel_raw)
+  json_formatted_string = json.dumps(json_object, indent=2)
+  print( json_formatted_string )
+
+#disconnect
+c_lib.disconnect_from_interface()
