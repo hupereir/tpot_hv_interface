1295340d8dd981a28b2ef35d731be5f47b77ba92
Hugo Pereira Da Costa <hugo.pereira.da.costa@gmail.com>
2023-07-17 12:00:57 -0400

    Added question bitmap to all questions. Added test on whether there is an ongoing run and warning dialog, before recovering FEE links

--- gui/tpot_hv_control.pl
+++ gui/tpot_hv_control.pl
@@ -16,7 +16,8 @@ sub tpot_go_off
     $reply = $mw->Dialog( 
         -text => 'This will turn OFF all TPOT HV channels. Confirm ?', 
         -title => 'TPOT HV OFF', 
-        -buttons => ['Yes', 'No'], -default_button => 'No' )->Show( -popover => 'cursor');
+ 	-bitmap => 'question',
+	-buttons => ['Yes', 'No'], -default_button => 'No' )->Show( -popover => 'cursor');
     if( uc($reply) eq "YES" )
     { $result = `$bin_path/tpot_hv_off.py --force all`; }
     $button_off->configure(-relief => 'raised' );
@@ -29,6 +30,7 @@ sub tpot_go_safe
     $reply = $mw->Dialog(
         -text => 'This will put TPOT in SAFE state. Confirm ?', 
         -title => 'TPOT HV SAFE', 
+	-bitmap => 'question',
         -buttons => ['Yes', 'No'], -default_button => 'No' )->Show( -popover => 'cursor');
     if( uc($reply) eq "YES" )
     {
@@ -45,6 +47,7 @@ sub tpot_go_operating
     $reply = $mw->Dialog(
         -text => 'This will turn ON all TPOT HV channels. Confirm ?', 
         -title => 'TPOT HV ON', 
+	-bitmap => 'question',
         -buttons => ['Yes', 'No'], -default_button => 'No' )->Show( -popover => 'cursor');
     if( uc($reply) eq "YES" )
     {
@@ -60,7 +63,8 @@ sub tpot_recover_trips
     $button_recover_trips->configure(-relief => 'sunken' );
     $reply = $mw->Dialog(
         -text => 'This will recover TPOT tripped channels. Confirm ?', 
-        -title => 'TPOT Recover Trips', 
+        -title => 'TPOT Recover Trips',
+	-bitmap => 'question',
         -buttons => ['Yes', 'No'], -default_button => 'Yes' )->Show( -popover => 'cursor');
     if( uc($reply) eq "YES" )
     {
@@ -72,11 +76,29 @@ sub tpot_recover_trips
 ##########################################3
 sub tpot_recover_fee_links
 {
+    # keep button sunken
     $button_recover_fee_links->configure(-relief => 'sunken' );
+
+    # check if TPOT vgtm is running. Print a warning if yes
+    my @result = split( ' ',`gl1_gtm_client gtm_fullstatus`);
+    my $vgtm = 12;
+    my $vgtm_is_running = hex($result[1]) & (1<<$vgtm);
+
+    if( $vgtm_is_running )
+    {
+	$reply = $mw->Dialog(
+	    -text => 'There seems to be a run ongoing. It is recommended to either stop the run or wait for the end of the current run before recovering lost fee links.', 
+	    -title => 'TPOT Recover FEE Links', 
+	    -bitmap => 'warning',
+	    -buttons => ['Ok'], -default_button => 'Ok')->Show( -popover => 'cursor');
+    }
+
+    # ask for confirmation
     $reply = $mw->Dialog(
         -text => 'This will recover TPOT FEE links. Confirm ?', 
         -title => 'TPOT Recover FEE Links', 
-        -buttons => ['Yes', 'No'], -default_button => 'Yes' )->Show( -popover => 'cursor');
+	-bitmap => 'question',
+        -buttons => ['Yes', 'No'], -default_button => 'Yes')->Show( -popover => 'cursor');
     if( uc($reply) eq "YES" )
     {
         $result = `$bin_lv_path/tpot_lv_recover_fee_links.py --force`;
