19bfb2edd3798e74890cc417c22632efc87f7e49
Hugo Pereira Da Costa <hugo.pereira.da.costa@gmail.com>
2023-09-07 10:14:27 -0400

    - Clarified HV and LV controls - added buttons to turn OFF Low Voltage and High Voltage

--- gui/tpot_hv_control.pl
+++ gui/tpot_hv_control.pl
@@ -10,22 +10,22 @@ $bin_path = "/home/phnxrc/operations/TPOT/tpot_hv_interface/python";
 $bin_lv_path = "/home/phnxrc/operations/TPOT/tpot_lv_interface";
 
 ##########################################3
-sub tpot_go_off
+sub tpot_hv_go_off
 {
-    $button_off->configure(-relief => 'sunken' );
+    $button_hv_off->configure(-relief => 'sunken' );
     $reply = $mw->Dialog( 
-        -text => 'This will turn OFF all TPOT HV channels. Confirm ?', 
+        -text => 'This will turn OFF TPOT High Voltage. Confirm ?', 
         -title => 'TPOT HV OFF', 
 	-buttons => ['Yes', 'No'], -default_button => 'No' )->Show( -popover => 'cursor');
     if( uc($reply) eq "YES" )
     { $result = `$bin_path/tpot_hv_off.py --force all`; }
-    $button_off->configure(-relief => 'raised' );
+    $button_hv_off->configure(-relief => 'raised' );
 }
 
 ##########################################3
-sub tpot_go_safe
+sub tpot_hv_go_safe
 {
-    $button_go_safe->configure(-relief => 'sunken' );
+    $button_hv_go_safe->configure(-relief => 'sunken' );
     $reply = $mw->Dialog(
         -text => 'This will put TPOT in SAFE state. Confirm ?', 
         -title => 'TPOT HV SAFE', 
@@ -35,13 +35,13 @@ sub tpot_go_safe
         $result = `$bin_path/tpot_hv_restore_state.py --force $config_path/tpot_hv_safe_state.json`;
         $result = `$bin_path/tpot_hv_on.py --force --mask $config_path/tpot_mask.json all`;
     }
-    $button_go_safe->configure(-relief => 'raised' );
+    $button_hv_go_safe->configure(-relief => 'raised' );
 }
 
 ##########################################3
-sub tpot_go_operating
+sub tpot_hv_go_operating
 {
-    $button_go_operating->configure(-relief => 'sunken' );
+    $button_hv_go_operating->configure(-relief => 'sunken' );
     $reply = $mw->Dialog(
         -text => 'This will turn ON all TPOT HV channels. Confirm ?', 
         -title => 'TPOT HV ON', 
@@ -51,13 +51,13 @@ sub tpot_go_operating
         $result = `$bin_path/tpot_hv_restore_state.py --force $config_path/tpot_hv_operating_state.json`;
         $result = `$bin_path/tpot_hv_on.py --force --mask $config_path/tpot_mask.json all`;
     }
-    $button_go_operating->configure(-relief => 'raised' );
+    $button_hv_go_operating->configure(-relief => 'raised' );
 }
 
 ##########################################3
-sub tpot_recover_trips
+sub tpot_hv_recover_trips
 {
-    $button_recover_trips->configure(-relief => 'sunken' );
+    $button_hv_recover_trips->configure(-relief => 'sunken' );
     $reply = $mw->Dialog(
         -text => 'This will recover TPOT tripped channels. Confirm ?', 
         -title => 'TPOT Recover Trips',
@@ -66,14 +66,32 @@ sub tpot_recover_trips
     {
         $result = `$bin_path/tpot_hv_recover_trips.py --force`;
     }
-    $button_recover_trips->configure(-relief => 'raised' );
+    $button_hv_recover_trips->configure(-relief => 'raised' );
 }
 
 ##########################################3
-sub tpot_recover_fee_links
+sub tpot_lv_go_off
 {
-    # keep button sunken
-    $button_recover_fee_links->configure(-relief => 'sunken' );
+    $button_lv_off->configure(-relief => 'sunken' );
+
+    # ask for confirmation
+    $reply = $mw->Dialog(
+        -text => 'This will turn OFF TPOT Low Voltage. Confirm ?', 
+        -title => 'TPOT LV OFF', 
+        -buttons => ['Yes', 'No'], -default_button => 'Yes')->Show( -popover => 'cursor');
+    if( uc($reply) eq "YES" )
+    {
+        $result = `$bin_lv_path/tpot_lv_off.py all`;
+    }
+    $button_lv_off->configure(-relief => 'raised' );
+}
+
+##########################################3
+sub tpot_lv_go_on
+{
+    # turning ON the LV is essentially the same as recovering fee links
+    # keep button sunken 
+    $button_lv_on->configure(-relief => 'sunken' );
 
     # check if TPOT vgtm is running. Print a warning if yes
     my @result = split( ' ',`gl1_gtm_client gtm_fullstatus`);
@@ -82,24 +100,58 @@ sub tpot_recover_fee_links
 
     if( $vgtm_is_running )
     {
-	$reply = $mw->Dialog(
-	    -text => 'There seems to be a run ongoing. Recovering FEE links requires to either stop the current run, or wait for the end of the current run.', 
-	    -title => 'TPOT Recover FEE Links', 
-	    -bitmap => 'warning',
-	    -buttons => ['Ok'], -default_button => 'Ok')->Show( -popover => 'cursor');
-	$button_recover_fee_links->configure(-relief => 'raised' );
-	return;
+        $reply = $mw->Dialog(
+            -text => 'There seems to be a run ongoing. Turning ON LV requires to either stop the current run, or wait for the end of the current run.', 
+            -title => 'TPOT LV ON', 
+            -bitmap => 'warning',
+            -buttons => ['Ok'], -default_button => 'Ok')->Show( -popover => 'cursor');
+        $button_lv_on->configure(-relief => 'raised' );
+        return;
     } else {
-	# ask for confirmation
-	$reply = $mw->Dialog(
-	    -text => 'This will recover TPOT FEE links. Confirm ?', 
-	    -title => 'TPOT Recover FEE Links', 
-	    -buttons => ['Yes', 'No'], -default_button => 'Yes')->Show( -popover => 'cursor');
-	if( uc($reply) eq "YES" )
-	{
-	    $result = `$bin_lv_path/tpot_lv_recover_fee_links.py --force`;
-	}
-	$button_recover_fee_links->configure(-relief => 'raised' );
+        # ask for confirmation
+        $reply = $mw->Dialog(
+            -text => 'This will turn ON TPOT Low Voltage. Confirm ?', 
+            -title => 'TPOT LV ON', 
+            -buttons => ['Yes', 'No'], -default_button => 'Yes')->Show( -popover => 'cursor');
+        if( uc($reply) eq "YES" )
+        {
+            $result = `$bin_lv_path/tpot_lv_recover_fee_links.py --force`;
+        }
+        $button_lv_on->configure(-relief => 'raised' );
+    }
+}
+
+##########################################3
+sub tpot_lv_recover_fee_links
+{
+    # keep button sunken
+    $button_lv_recover_fee_links->configure(-relief => 'sunken' );
+
+    # check if TPOT vgtm is running. Print a warning if yes
+    my @result = split( ' ',`gl1_gtm_client gtm_fullstatus`);
+    my $vgtm = 12;
+    my $vgtm_is_running = hex($result[1]) & (1<<$vgtm);
+
+    if( $vgtm_is_running )
+    {
+        $reply = $mw->Dialog(
+            -text => 'There seems to be a run ongoing. Recovering FEE links requires to either stop the current run, or wait for the end of the current run.', 
+            -title => 'TPOT Recover FEE Links', 
+            -bitmap => 'warning',
+            -buttons => ['Ok'], -default_button => 'Ok')->Show( -popover => 'cursor');
+        $button_lv_recover_fee_links->configure(-relief => 'raised' );
+        return;
+    } else {
+        # ask for confirmation
+        $reply = $mw->Dialog(
+            -text => 'This will recover TPOT FEE links. Confirm ?', 
+            -title => 'TPOT Recover FEE Links', 
+            -buttons => ['Yes', 'No'], -default_button => 'Yes')->Show( -popover => 'cursor');
+        if( uc($reply) eq "YES" )
+        {
+            $result = `$bin_lv_path/tpot_lv_recover_fee_links.py --force`;
+        }
+        $button_lv_recover_fee_links->configure(-relief => 'raised' );
     }
 }
 
@@ -129,30 +181,43 @@ $smallfont = $mw->fontCreate('code', -family => 'fixed',-size => 10);
 
 $mw->title("TPOT LV and HV Control");
 
-$label{'sline'} = $mw->Label(-text => "TPOT LV and HV Control", -background=>$slinebg, -font =>$bigfont);
-$label{'sline'}->pack(-side=> 'top', -fill=> 'x', -ipadx=> '15m', -ipady=> '1m');
+# HV controls
+$label = $mw->Label(-text => "TPOT High Voltage (HV) Control", -background=>$slinebg, -font =>$bigfont);
+$label->pack(-side=> 'top', -fill=> 'x', -ipadx=> '15m', -ipady=> '1m');
 
-$frame{'center'} = $mw->Frame()->pack(-side => 'top', -fill => 'x');
-$framebutton = $frame{'center'}->Label(-bg => $color1, -relief=> 'raised')->pack(-side =>'top', -fill=> 'x', -ipadx=>'15m');
-
-$button_off = $framebutton->
-    Button(-bg => $buttonbgcolor, -text => "Turn OFF", -command => [\&tpot_go_off, $b],  -relief =>'raised',  -font=> $normalfont)->
+$framebutton = $mw->Frame()->pack(-side => 'top', -fill => 'x');
+$button_hv_off = $framebutton->
+    Button(-bg => $buttonbgcolor, -text => "Turn OFF  High Voltage", -command => [\&tpot_hv_go_off, $b],  -relief =>'raised',  -font=> $normalfont)->
     pack(-side =>'top', -fill=> 'x', -ipadx=> '1m',  -ipady=> '1m');
 
-$button_go_safe = $framebutton->
-    Button(-bg => $buttonbgcolor, -text => "Go to SAFE", -command => [\&tpot_go_safe, $b],  -relief =>'raised',  -font=> $normalfont)->
+$button_hv_go_safe = $framebutton->
+    Button(-bg => $buttonbgcolor, -text => "Go to SAFE", -command => [\&tpot_hv_go_safe, $b],  -relief =>'raised',  -font=> $normalfont)->
     pack(-side =>'top', -fill=> 'x', -ipadx=> '1m',  -ipady=> '1m');
 
-$button_go_operating = $framebutton->
-    Button(-bg => $buttonbgcolor, -text => "Turn ON", -command => [\&tpot_go_operating, $b],  -relief =>'raised',  -font=> $normalfont)->
+$button_hv_go_operating = $framebutton->
+    Button(-bg => $buttonbgcolor, -text => "Turn ON High Voltage", -command => [\&tpot_hv_go_operating, $b],  -relief =>'raised',  -font=> $normalfont)->
     pack(-side =>'top', -fill=> 'x', -ipadx=> '1m',  -ipady=> '1m');
 
-$button_recover_trips = $framebutton->
-    Button(-bg => $buttonbgcolor, -text => "Recover High Voltage trips", -command => [\&tpot_recover_trips, $b],  -relief =>'raised',  -font=> $normalfont)->
+$button_hv_recover_trips = $framebutton->
+    Button(-bg => $buttonbgcolor, -text => "Recover High Voltage trips", -command => [\&tpot_hv_recover_trips, $b],  -relief =>'raised',  -font=> $normalfont)->
     pack(-side =>'top', -fill=> 'x', -ipadx=> '1m',  -ipady=> '1m');
 
-$button_recover_fee_links = $framebutton->
-    Button(-bg => $buttonbgcolor, -text => "Recover FEE links", -command => [\&tpot_recover_fee_links, $b],  -relief =>'raised',  -font=> $normalfont)->
+# LV controls
+$label = $mw->Label(-text => "TPOT Low Voltage (LV) Control", -background=>$slinebg, -font =>$bigfont);
+$label->pack(-side=> 'top', -fill=> 'x', -ipadx=> '15m', -ipady=> '1m');
+
+$framebutton= $mw->Frame()->pack(-side => 'top', -fill => 'x');
+
+$button_lv_off = $framebutton->
+    Button(-bg => $buttonbgcolor, -text => "Turn OFF Low Voltage", -command => [\&tpot_lv_go_off, $b],  -relief =>'raised',  -font=> $normalfont)->
+    pack(-side =>'top', -fill=> 'x', -ipadx=> '1m',  -ipady=> '1m');
+
+$button_lv_on = $framebutton->
+    Button(-bg => $buttonbgcolor, -text => "Turn ON Low Voltage", -command => [\&tpot_lv_go_on, $b],  -relief =>'raised',  -font=> $normalfont)->
+    pack(-side =>'top', -fill=> 'x', -ipadx=> '1m',  -ipady=> '1m');
+
+$button_lv_recover_fee_links = $framebutton->
+    Button(-bg => $buttonbgcolor, -text => "Recover FEE links", -command => [\&tpot_lv_recover_fee_links, $b],  -relief =>'raised',  -font=> $normalfont)->
     pack(-side =>'top', -fill=> 'x', -ipadx=> '1m',  -ipady=> '1m');
 
 MainLoop();
